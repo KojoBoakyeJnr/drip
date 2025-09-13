cfd3a712438b0ff8442d57f7bd3e62cfb891d1bc
KojoBoakye <kojoboakyejnr@outlook.com>
2025-09-13 01:27:26 +0000

    adding colors and logos

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,17 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(theme: ThemeData(), home: Scaffold());
+    return MaterialApp(
+      theme: ThemeData(),
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        body: Center(
+          child: Container(
+            width: 100,
+            child: Image.asset("assets/icon/icon.png"),
+          ),
+        ),
+      ),
+    );
   }
 }
---
+++ lib/utils/app_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color mainBlack = Color(0x1b1b1b);
+  static const Color mainGreen = Color.fromRGBO(176, 255, 19, 1);
+  static const Color mainWhite = Color.fromRGBO(255, 255, 255, 1);
+}
